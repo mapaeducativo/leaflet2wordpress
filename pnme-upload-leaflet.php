22ca46250cf26d5ac426696551ecdae6afcdc6d0
nax89 <cruz.nazareno.89@gmail.com>
2015-05-14 18:43:06 -0300

    Terminada la primera versión del plugin

--- pnme-upload-leaflet.php
+++ pnme-upload-leaflet.php
@@ -16,11 +16,6 @@ function my_plugin_menu() {
     add_submenu_page( 'leaflet2wordpress_uploads', 'Mapas Subidos', 'Mapas Subidos', 'manage_options', 'leaflet2wordpress_lists', 'l2w_list_page' );
 }
 
-function l2w_list_page() {	
-	echo '<div class="wrap"><div id="icon-tools" class="icon32"></div>';
-		echo '<h2>My Custom Submenu Page</h2>';
-	echo '</div>';
-}
 
 function l2w_upload_page(){
 ?>
@@ -109,7 +104,7 @@ function l2w_upload_page(){
             
             if ( false == $archivo_error ){
                 $descomprimir = true;
-                $msjs['update-nag'][] = "Los archivos comprimidos son válidos. ✔";
+                $msjs['updated'][] = "Los archivos comprimidos son válidos. ✔";
             }
             else{
                 $msjs['error'][] = "El zip contiene archivos no válidos.";
@@ -151,6 +146,10 @@ function l2w_upload_page(){
                 $unzipfile = unzip_file( $filedest, $upload_dir['path'].$dirname);
                    if ( $unzipfile ) {
                       $msjs['updated'][] = 'Archivos extraidos correctamente!';
+                      // Descomentando esta funcion se eliminan los zips una vez extraido el mapa. 
+                      // unlink($filedest);
+                      $msjs['updated'][] = 'Mapa subido.<br>'
+                              . 'Puede ver el Mapa en el siguiente enlace: <a target="_blank" href="'.$upload_dir['url'].$dirname.'">'.$upload_dir['url'].$dirname.'</a>';
                    } else {
                       $msjs['error'][] = 'Error al descomprimir el archivo';  
                    }
@@ -162,12 +161,12 @@ function l2w_upload_page(){
     
     <?php l2w_show_messages(); ?>
     <?php // print_r($msjs); ?>
-        <h4>Suba el .zip que contiene los archivo del Mapa Leaflet</h4>
+        <h4>Suba el .zip que contiene los archivos del Mapa Leaflet</h4>
         <form name="uploadfile" id="uploadfile_form" method="POST" enctype="multipart/form-data"  accept-charset="utf-8" >
             <?php // genero un campo "nonce" para seguridad ?>
             <?php wp_nonce_field( 'uploadingzip' ); ?>
             <input type="file" name="uploadfiles" id="uploadfiles" size="35" class="uploadfiles" />
-            <input type="text" name="dirname" id="dirname" size="35" class="" placeholder="nombre_del_mapa"/>
+            <input type="text" name="dirname" id="dirname" size="35" class="" placeholder="nombre_del_mapa" required/>
             <input class="button-primary" type="submit" name="uploadfile" id="uploadfile_btn" value="Upload"  />
             <p>Escriba un nombre para el mapa, solo se permiten letras minusculas y numeros. Sin espacios.</p>
         </form>
@@ -176,14 +175,104 @@ function l2w_upload_page(){
 <?php
 }
 
+function l2w_list_page() {	
+    global $msjs; ?>
+    <?php if (current_user_can("manage_options")) : ?>
+    <div class="wrap"><div id="icon-tools" class="icon32"></div>
+    <h2>Mapas Leaflet</h2>
+    <br class="clear">
+<?php 
+        // Obtengo el directorio para las descargas /uploads/leaflet/...
+        add_filter('upload_dir', 'l2w_my_upload_dir');
+        $upload_dir = wp_upload_dir();
+        remove_filter('upload_dir', 'l2w_my_upload_dir');
+        
+        // Eliminar mapa si existe action=delete y existe un name
+        if (wp_verify_nonce( $_GET['_wpnonce'],'borramapa' ) && 'delete' == $_GET['action'] && '' != $_GET['name']) {
 
+            $aeliminar = $upload_dir['path'].sanitize_key($_GET['name']);
+            if ( rrmdir($aeliminar) ){
+                $msjs['updated'][] = "El mapa se ha eliminado correctamente.";
+            }
+            else{
+                $msjs['error'][] = "Se ha producido un error al intentar eliminar el mapa.";
+            }
+        } // verifica nonce y accion 
+        
+        l2w_show_messages();
+        
+        $carpetas = scandir($upload_dir['path']);
+        // echo $upload_dir['path'];
+        // print_r($carpetas);
+        // print_r($upload_dir);
+        ?>
+        <table class="wp-list-table widefat fixed posts">
+            <thead><tr><th class="manage-column" scope="col">Nombre</th><th class="manage-column" scope="col">URL</th><th>Codigo Embebido</th></tr></thead>
+        <tbody id="the-list">
+        <?php 
+        $i = 0;
+        foreach ($carpetas as $c){
+            if ( '.' != $c && '..' != $c){
+                $alt = ( $i % 2 != 0 ) ? "alt" : "";
+                if (is_dir($upload_dir['path'].$c)){
+                    echo '<tr class="'.$alt.' '.$i.'"><td><strong>'.$c.'</strong>'
+                            . '<div class="row-actions">'
+                                . '<span class="edit"><a title="Editar" href="#action=edit">Editar</a> | </span>'
+                            . '<span class="trash"><a href="'.wp_nonce_url(admin_url('admin.php?page=leaflet2wordpress_lists&action=delete&name='.$c), 'borramapa').'" title="Borrar" class="submitdelete">Borrar</a> </span>'
+                            . '</div></td>'
+                            . '<td><a target="_blank" href="'.$upload_dir['url'].$c.'">'.$upload_dir['url'].$c.'</a></td>'
+                            . '<td><textarea cols="50">[embed]'.$upload_dir['url'].$c.'[/embed]</textarea></td></tr>';
+                    $i++;
+                }
+            }
+        }
+        ?>
+            </tbody>
+        </table>
+    </div>
+<?php
+    endif; // if can manage options
+}
+
+/*
+ * Oembed Leaflets
+ * Funcion para agregar el embebido automatico de direcciones que contengan /uploads/leaflet/ en su URL
+ */
+
+add_action("init","l2w_embed_handlers");
+
+function l2w_embed_handlers(){
+    wp_embed_register_handler( 'l2w_leaflet', '#(http://[a-z./_-]*uploads\/leaflet\/[\d,\w,\s,-]+)($|&)#i', 'wp_embed_handler_l2w_wordpress' );
+}
+
+
+function wp_embed_handler_l2w_wordpress( $matches, $attr, $url, $rawattr ) {
+
+	$embed = sprintf(
+			'<iframe class="media media-object map" src="%1$s" width="900" style="max-width:100%;height:400px;" height="500" frameborder="0" scrolling="no" marginwidth="0" marginheight="0"></iframe><div class="map_link"><a href="%1$s/" target="_blank" title="Ver mapa completo">Ver mapa completo</a></div>',
+			esc_attr($matches[1])
+			);
+
+	return apply_filters( 'embed_l2w_leaflet', $embed, $matches, $attr, $url, $rawattr );
+}
+
+/*
+ * Helpers
+ */
+
+/*
+ * Funcion para agregar el /leaflet/ a la direccion de uploads.
+ */
 function l2w_my_upload_dir($upload) {
 	$upload['subdir']	= '/leaflet/';
 	$upload['path']		= $upload['basedir'] . $upload['subdir'];
 	$upload['url']		= $upload['baseurl'] . $upload['subdir'];
 	return $upload;
 }
-
+/*
+ * Funcion para mostrar los msjs del array $msjs
+ * 
+ */
 function l2w_show_messages(){
     global $msjs;
     /*
@@ -198,4 +287,22 @@ function l2w_show_messages(){
         <?php
         }
     }
+}
+/*
+ * Funcion para borrar recursivamente un directorio
+ * Return true|false
+ */
+if ( !function_exists("rrmdir") ){
+    function rrmdir($dir) { 
+       if (is_dir($dir)) { 
+         $objects = scandir($dir); 
+         foreach ($objects as $object) { 
+           if ($object != "." && $object != "..") { 
+             if (filetype($dir."/".$object) == "dir") rrmdir($dir."/".$object); else unlink($dir."/".$object); 
+           } 
+         } 
+         reset($objects); 
+         return rmdir($dir); 
+       } 
+     }
 }
--- readme.txt
+++ readme.txt
@@ -1,5 +1,5 @@
-=== Plugin Name ===
-Contributors:
+=== Leaflet2Wordpress ===
+Contributors: Naza, Telle
 Donate link: 
 Tags: 
 Requires at least: 
@@ -10,11 +10,14 @@ License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
 
 == Description ==
-
+Permite subir un zip con archivos de Leaflet (generados en Qgis), generando un link que se puede insertar o embeber en una entrada. 
 
 == Installation ==
-
+Instalar el plugin.
+Subir Zip en la seccion "Subir Mapa Leaflet".
+Buscar el link generado en la seccion "Mapas Subidos".
 
 == Changelog ==
 
 = 1.0 =
+Primera version del plugin, permite subir zips y obtener el link de la carpeta extraida.
